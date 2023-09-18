45739ca7b1def6c8f5ad4d4afe0965d0b84784d5
CongLuu1212 <142959339+CongLuu1212@users.noreply.github.com>
2023-09-18 10:24:43 +0700

    Add files via upload

---
+++ menu tea.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void menu_Korea_Tea();
+
+int main() {
+	menu_Korea_Tea();
+}
+void menu_Korea_Tea() {
+	printf("\n\n ^Korea Tea^");
+	printf("\n Menu :");
+	printf("\n 1. Tra sua truyen thong :   25k");
+	printf("\n 2. Tra xanh matcha :        30k");
+	printf("\n 3. Tra hoa cuc :            20k");
+	printf("\n 4. Tra mat ong bac ha :     40k");
+	printf("\n 5. Tra o long :             20k");
+	printf("\n 6. Matcha da xay cho bbi :  Free ");
+}
+	
